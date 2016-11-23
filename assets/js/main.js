af0c696a896d004a0fd885018b761d60b9616314
Jeffrey LeCompte <jeffreylec@gmail.com>
2016-11-23 15:15:34 -0800

    fixed open graph and fb sdk

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,20 +116,3 @@ $(document).ready(function () {
             .text("you have " + time + " minutes of free time");
     });
 });
-
-// for facebook
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '201872290265624',
-        xfbml      : true,
-        version    : 'v2.8'
-    });
-};
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
